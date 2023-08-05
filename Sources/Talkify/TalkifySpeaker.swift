adc61c1143ab0c751359c72cab75e372ef65ab1d
tornikegomareli <gomarelidevelopment@gmail.com>
2023-08-05 17:13:25 +0400

    TalkifySpeaker docs

--- Sources/Talkify/TalkifySpeaker.swift
+++ Sources/Talkify/TalkifySpeaker.swift
@@ -1,6 +1,5 @@
-//
 //  TalkifySpeakingDelegate.swift
-//  
+//
 //
 //  Created by Tornike on 05/08/2023.
 //
@@ -8,41 +7,61 @@
 import Foundation
 import AVFAudio
 
+/// A macOS 10.15+ class that speaks text using `AVSpeechSynthesizer`.
+/// Allows configuration of voice, rate, pitch multiplier, and volume.
+/// Speaking events are reported to an optional delegate.
 @available(macOS 10.15, *)
 public class TalkifySpeaker {
+  /// The object that acts as the delegate of the `TalkifySpeaker`.
   public weak var delegate: TalkifySpeakingDelegate?
+
   private var synthesizer = AVSpeechSynthesizer()
   private var voice = TalkifyVoice()
   private var voiceRate: Float = 0.5
   private var multiplier: Float = 0.5
   private var volume: Float = 1.0
 
+  /// Creates a new `TalkifySpeaker`.
   public init() {}
 
+  /// Configures the voice of the speaker.
+  /// - Parameter customVoice: The custom voice to be used.
+  /// - Returns: A self-reference, enabling chained calls.
   @discardableResult
   public func withVoice(customVoice: TalkifyVoice) -> Self {
     voice = customVoice
     return self
   }
 
+  /// Configures the speaking rate of the speaker.
+  /// - Parameter value: The rate at which the speaker should speak.
+  /// - Returns: A self-reference, enabling chained calls.
   @discardableResult
   public func withVoiceRate(value: Float) -> Self {
     voiceRate = value
     return self
   }
 
+  /// Configures the pitch multiplier of the speaker.
+  /// - Parameter value: The multiplier for the pitch of the speaker's voice.
+  /// - Returns: A self-reference, enabling chained calls.
   @discardableResult
   public func withMultiplier(value: Float) -> Self {
     multiplier = value
     return self
   }
 
+  /// Configures the volume of the speaker.
+  /// - Parameter value: The volume at which the speaker should speak.
+  /// - Returns: A self-reference, enabling chained calls.
   @discardableResult
   public func withVolume(value: Float) -> Self {
     volume = value
     return self
   }
 
+  /// Starts speaking the provided text.
+  /// - Parameter text: The text to be spoken.
   public func startSpeaking(with text: String) {
     let utterance = TalkifyUtternace(string: text)
     utterance.setupTalkifyValues(customVoice: voice, customRate: voiceRate, customMultiplier: multiplier, customVolume: volume)
@@ -50,6 +69,7 @@ public class TalkifySpeaker {
     delegate?.speakingDidStart()
   }
 
+  /// Stops any ongoing speech immediately.
   public func stopSpeaking() {
     guard synthesizer.isSpeaking || synthesizer.isPaused else {
       return
@@ -58,6 +78,7 @@ public class TalkifySpeaker {
     delegate?.speakingDidStop()
   }
 
+  /// Pauses any ongoing speech immediately.
   public func pauseSpeaking() {
     guard synthesizer.isSpeaking else {
       return
@@ -66,6 +87,7 @@ public class TalkifySpeaker {
     delegate?.speakingDidPause()
   }
 
+  /// Continues any paused speech.
   public func continueSpeaking() {
     guard synthesizer.isPaused else {
       return
