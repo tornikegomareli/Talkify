5ab3c34d118ddd45f0a6a75e7556bda3c8334ad9
tornikegomareli <gomarelidevelopment@gmail.com>
2026-08-17 02:03:42 +0400

    Release 0.5.0

--- Casks/talkify.rb
+++ Casks/talkify.rb
@@ -1,6 +1,6 @@
 cask "talkify" do
-  version "0.4.2"
-  sha256 "f28e5bf85e45debf2f432aa383d2e5761be341acaa5f90313d053e55e1baa927"
+  version "0.5.0"
+  sha256 "0acc636a9cecf5ca91da772e5e7579b8cbce86a72995923a38193b10e1be84da"
 
   url "https://github.com/tornikegomareli/Talkify/releases/download/v#{version}/Talkify.dmg",
       verified: "github.com/tornikegomareli/Talkify/"
